01a90dbf5d0cc98f0ac94252a1084451d2a39ef7
GritcoNicoleta <gritconicoleta@mail.ru>
2021-12-05 21:06:20 +0200

    first commit

---
+++ lab2/lab2/Broker/Program.cs
@@ -0,0 +1,32 @@
+﻿using Services;
+using System;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Broker is running!");
+
+            IService broker = new BrokerService();
+
+            Task t = Task.Factory.StartNew(async () =>
+            {
+                string message;
+
+                while ((message = await broker.AsyncRead()) != "quit b")
+                {
+                    Console.WriteLine(message);
+                    await broker.AsyncWrite(message);
+                    await broker.AsyncReload();
+                }
+            });
+
+            t.Wait();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lab2/lab2/Models/Receiver.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace Models
+{
+    public class Receiver
+    {
+        public User User { get; set; }
+        public IPEndPoint IpEndPoint { get; set; }
+    }
+}
