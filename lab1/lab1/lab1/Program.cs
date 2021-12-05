068e0c620dd4254e75d2e5202f05a0af7f261477
GritcoNicoleta <gritconicoleta@mail.ru>
2021-12-05 21:05:30 +0200

    first commit

---
+++ lab1/lab1/lab1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lab1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var t1 = new MyThread("Thread 1", GetRandomNumber(3000));
+            var t2 = new MyThread("Thread 2", GetRandomNumber(3000));
+            var t3 = new MyThread("Thread 3", GetRandomNumber(3000));
+            var t4 = new MyThread("Thread 4", GetRandomNumber(3000));
+            var t5 = new MyThread("Thread 5", GetRandomNumber(3000));
+            var t6 = new MyThread("Thread 6", GetRandomNumber(3000));
+            var t7 = new MyThread("Thread 7", GetRandomNumber(3000));
+
+            t5.WaitFor(t1, t2);
+            t6.WaitFor(t3, t4);
+            t7.WaitFor(t5, t6);
+            
+
+            MyThread[] threads = { t1, t2, t3, t4, t5, t6, t7 };
+
+            foreach (var thread in threads) thread.Start();
+        }
+
+        private static int GetRandomNumber(int max) => new Random().Next(max);
+    }
+}
